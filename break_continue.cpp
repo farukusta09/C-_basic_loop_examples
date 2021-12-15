755b9ba57665422e49e98f2731e9bf2611ecca1a
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2021-12-15 13:18:12 +0300

    Create break_continue.cpp

---
+++ break_continue.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+  int main()
+{
+     for(int i=0;i<5;i++)
+     {
+        if(i==3)
+       {
+          break;
+       }
+       cout<<i<<endl;
+     }
+
+  int j=0;
+while(j<6)
+{
+  if(j==3)
+  {
+    j++;
+  continue;
+ }
+ cout<<j<<endl;
+ j++;
+ }
+
+}
