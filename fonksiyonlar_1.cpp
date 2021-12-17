daf32563cbb6c1ce293916f489868007ce58ec5c
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2021-12-17 08:55:22 +0300

    Create fonksiyonlar_1.cpp

---
+++ fonksiyonlar_1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void topla(int a, int b) {
+	cout << "toplam:" << a + b << endl;
+}
+
+void ekranaMerhabaYazdır() {
+	cout << "merhaba" << endl;
+}
+
+
+
+
+int main() {
+
+	topla(5,7);
+
+ekranaMerhabaYazdır();
+
+
+}
