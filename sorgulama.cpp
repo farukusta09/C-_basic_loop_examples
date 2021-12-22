428e18df11c97eb30b0f66febdfe3be5136b7104
Ömer Faruk USTA <92623366+farukusta09@users.noreply.github.com>
2021-12-22 16:07:52 +0300

    Create sorgulama.cpp

---
+++ sorgulama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int grade;
+	cout << "Enter the grade:";
+	cin >> grade;
+
+	grade > 50 ? cout << "Student passes" : cout << "Student fails";
+
+
+}
